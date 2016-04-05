9a9b74c31c54fee22fa7146e07b4b3cf1f3c2aa9
Therese Dachille <tcd12@pitt.edu>
2016-04-05 15:32:37 -0400

    commented test cases

--- Deliverable5.java
+++ Deliverable5.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 public class Deliverable5 {
 
+	//Tests that original and refactored convertToInt() produce same output with base cases
 	@Test
 	public void convertToIntTestBaseInt() {
 		MainPanel mp = new MainPanel(15);
@@ -13,6 +14,8 @@ public class Deliverable5 {
 		}
 	}
 	
+	//Tests that original and refactored convertToInt() produce DIFFERENT output with edge case of negative values
+	//(they are not equal because the original convertToInt() has an error with negative numbers)
 	@Test
 	public void convertToIntTestNegative() {
 		MainPanel mp = new MainPanel(15);
@@ -32,35 +35,32 @@ public class Deliverable5 {
 		
 	}
 	
+	//Tests that original and refactored convertToInt() produce same output with edge case of zero
 	@Test
 	public void convertToIntTestZero() {
 		MainPanel mp = new MainPanel(15);
 		assertEquals(mp.convertToInt(0), mp.originalConvertToInt(0));
 	}
 	
+	//Tests that original and refactored convertToInt() produce same output with edge case of max_value
 	@Test
 	public void convertToIntTestMax() {
 		MainPanel mp = new MainPanel(15);
 		assertEquals(mp.convertToInt(Integer.MAX_VALUE), mp.originalConvertToInt(Integer.MAX_VALUE));
 	}
 	
+	//Tests that original and refactored Cell.toString() produce same output when cells are alive
 	@Test
 	public void CellToStringTestAlive() {
 		Cell cell = new Cell(true);
 		assertEquals(cell.toString(), cell.originalToString());
 	}
 	
+	//Tests that original and refactored Cell.toString() produce same output when cells are not alive
 	@Test
 	public void CellToStringTestAliveFalse() {
 		Cell cell = new Cell();
 		assertEquals(cell.toString(), cell.originalToString());
-	}
-	
-	//////////////////////////ADD RUNCONTINUOUS TESTS/////////////////////////////////
-	@Test
-	public void runContinuousTest() {
-		MainPanel mp = new MainPanel(15);
-		
 	}
 
 }
